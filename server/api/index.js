1f3fbf6d0555a49cb96e71ccf9552135198bf01a
Simon Eddy <simon@simoneddy.com.au>
2024-05-10 15:08:03 +1000

    simulation server route, additional data

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const factoryRouter = require('./factory');
+const simulationRouter = require('./simulation');
 
 const apiRouter = express.Router()
 
 apiRouter.use('/factory', factoryRouter)
+apiRouter.use('/simulation', simulationRouter)
 
 module.exports = apiRouter;
---
+++ server/api/simulation/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const prepareMatchSimulation = require('../../../build/core/bootstrap/prepareMatchSimulation').default;
+const {
+  matchupFactory,
+  playerFactory,
+  leagueFactory,
+  fixtureFactory
+} = require('../../../build/factories');
+const { Kernel } = require('../../../build/core');
+
+const simulationRouter = express.Router()
+
+simulationRouter.get('/match', async (_req, res) => {
+  const kernel = Kernel.init()
+  const matchup = matchupFactory()
+  const sim = prepareMatchSimulation(matchup, kernel)
+  const result = await Promise.resolve(() => {
+    const result = kernel.run(sim)
+    return result
+  })
+  return res.json(result())
+})
+
+module.exports = simulationRouter;
